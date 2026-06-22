8abac88aedb5a1e4bc9b7192b84e560e621f41d0
aljwaal1 <yaya15112016@gmail.com>
2026-06-22 08:33:37 +0300

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,14 +1,1 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-const CACHE_NAME = "iphone-tasks-pwa-pro-v6";
-const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).catch(()=>{}));
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-});
